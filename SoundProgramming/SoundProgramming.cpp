8a3cc402cb0f229579074e60cfe15431348b1066
Raffaele Ariano <raffaele.aariano@gmail.com>
2022-08-29 16:38:06 +0200

    added main loop

--- SoundProgramming/SoundProgramming.cpp
+++ SoundProgramming/SoundProgramming.cpp
@@ -2,13 +2,18 @@
 #include "fmod.h"
 #include "fmod.hpp"
 #include "fmod_errors.h"
+
+#include "common.h"
 #include <iostream>
 
 
-int main()
+int FMOD_Main()
 {
 	FMOD_RESULT result;
 	FMOD::System* system = NULL;
+
+	
+	Common_Init(0);
 
 	result = FMOD::System_Create(&system);      // Create the main system object.
 	if (result != FMOD_OK)
@@ -17,11 +22,22 @@ int main()
 		exit(-1);
 	}
 
-	result = system->init(512, FMOD_INIT_NORMAL, 0);    // Initialize FMOD.
+	result = system->init(32, FMOD_INIT_NORMAL, 0);    // Initialize FMOD.
 	if (result != FMOD_OK)
 	{
 		printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
 		exit(-1);
 	}
+
+	/*
+	   Main loop.
+   */
+	do
+	{
+		Common_Update();
+
+		Common_Draw("Press %s to quit", Common_BtnStr(BTN_QUIT));
+	} while (!Common_BtnPress(BTN_QUIT));
+
 	return 0;
 }
